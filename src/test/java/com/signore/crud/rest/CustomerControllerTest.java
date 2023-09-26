aa2649924d1112d6573e58e50eb97f60261de313
nicholassignore <nicholas.santos@live.de>
2023-09-26 17:45:57 +0200

    bla

--- src/test/java/com/signore/crud/rest/CustomerControllerTest.java
+++ src/test/java/com/signore/crud/rest/CustomerControllerTest.java
@@ -2,10 +2,16 @@ package com.signore.crud.rest;
 
 import com.signore.crud.model.Customer;
 import com.signore.crud.repositories.CustomerRepository;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -17,8 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
+@ContextConfiguration(initializers = {CustomerControllerTest.Initializer.class})
 class CustomerControllerTest {
 
     @Autowired
@@ -27,11 +39,32 @@ class CustomerControllerTest {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
+    }
+
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+            .withDatabaseName("crudrest")
+            .withUsername("crudrest")
+            .withPassword("crudrest");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+
     @Test
     void getCustomers() throws Exception {
 
-        // puliamo la tabella dei customer.
-        customerRepository.deleteAll();
 
         // aggiungo nicholas nel DB
         customerRepository.save(new Customer(1L, "nicholas", "signore"));
