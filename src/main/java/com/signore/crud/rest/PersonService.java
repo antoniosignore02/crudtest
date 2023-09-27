2fee103de7ca6915cad5c80872738ba2ba93fb54
Antonio Signore <signant@hilti.com>
2023-09-27 18:05:27 +0200

    deleted .idea

--- src/main/java/com/signore/crud/rest/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.signore.crud.rest;
-
-import com.signore.crud.model.Customer;
-
-import java.util.List;
-
-public interface PersonService {
-    List<Customer> searchByFirstAndOrLastName(String firstName, String lastName);
-}
